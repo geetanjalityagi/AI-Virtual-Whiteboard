5765c0cb854d2770708d4eacc0b7b54aad238575
geetanjalityagi <geetanjalityagi1057@gmail.com>
2026-08-06 01:03:33 +0530

    Add toolbar

--- VisionBoard.py
+++ VisionBoard.py
@@ -2,10 +2,14 @@ import cv2
 import time
 from HandTrackingModule import handDetector
 
+
 cap = cv2.VideoCapture(1)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+toolbar = cv2.imread("assests/Colors.jpg")
+toolbar = cv2.resize(toolbar, (1280, 150))
 
 detector = handDetector()
 
@@ -18,13 +22,15 @@ while(True):
 
     img = cv2.flip(img, 1)
 
+    img[0:150, 0:1280] = toolbar
+
     img = detector.findhands(img)
 
     ctime = time.time()
     fps = 1/(ctime - ptime)
     ptime = ctime
 
-    cv2.putText(img, f"FPS : {fps : .2f}", (40, 70), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 1)
+    cv2.putText(img, f"FPS : {fps : .2f}", (40, 180), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 1)
     cv2.imshow("img", img)
 
     if cv2.waitKey(1) == ord('q'):
