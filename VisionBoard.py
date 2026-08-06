71a4d8a1dd37a98ba10e9c6f64fa84f9fe1b3f2e
geetanjalityagi <geetanjalityagi1057@gmail.com>
2026-08-07 01:28:24 +0530

    Implement toolbar selection functionality

--- VisionBoard.py
+++ VisionBoard.py
@@ -1,19 +1,24 @@
 import cv2 
 import time
 from HandTrackingModule import handDetector
+from toolbar import select_toolbar_tool
 
 
+# Try opening camera 0 first, fall back to 1 if it fails
 cap = cv2.VideoCapture(1)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-toolbar = cv2.imread("assests/Colors.jpg")
-toolbar = cv2.resize(toolbar, (1280, 150))
+toolbar = cv2.imread("assets/Colors.jpg")
+if toolbar is not None:
+    toolbar = cv2.resize(toolbar, (1280, 150))
 
 detector = handDetector(num_hands=1)
 
 colorBar = (255, 0, 0)
+brushThickness = 15
+eraserThickness = 80
 
 ptime = 0
 while(True):
@@ -24,23 +29,37 @@ while(True):
 
     img = cv2.flip(img, 1)
 
-    img[0:150, 0:1280] = toolbar
-
     img = detector.findhands(img)
     lmlist = detector.findpoints(img)
 
     if len(lmlist) != 0:
+        # Tip of index finger
         x1, y1 = lmlist[8][1], lmlist[8][2]
+        # Tip of middle finger
         x2, y2 = lmlist[12][1], lmlist[12][2]
 
+        # 1. Selection Mode: Both index and middle fingers are up
         if (lmlist[8][2] < lmlist[6][2]) and (lmlist[12][2] < lmlist[10][2]):
-            cv2.rectangle(img, (x1, y1), (x2,y2), colorBar, -1)
+            cv2.rectangle(img, (x1, y1 - 25), (x2, y2 + 25), colorBar, cv2.FILLED)
+            
+            # Select color/tool
+            new_color, new_toolbar = select_toolbar_tool(x1, y1)
+            if new_color is not None:
+                colorBar = new_color
+                if new_toolbar is not None:
+                    toolbar = new_toolbar
 
-        elif(lmlist[8][2] < lmlist[6][2]):
-            cv2.circle(img, (x1, y1), 12, (255, 0, 255), 3)
+        # 2. Drawing Mode: Only index finger is up
+        elif (lmlist[8][2] < lmlist[6][2]):
+            cv2.circle(img, (x1, y1), 20, colorBar, cv2.FILLED)
+
+
+    # Overlay the toolbar at the top
+    if toolbar is not None:
+        img[0:150, 0:1280] = toolbar
 
     ctime = time.time()
-    fps = 1/(ctime - ptime)
+    fps = 1/(ctime - ptime) if (ctime - ptime) > 0 else 0
     ptime = ctime
 
     cv2.putText(img, f"FPS : {fps : .2f}", (40, 180), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 1)
